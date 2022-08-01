dc1b207aaaeef9f2e7b5f298b64f40a0ba16b28a
Saumya Srivastava <76783291+saumya-srivastava226@users.noreply.github.com>
2022-08-01 18:48:44 +0530

    Add files via upload

---
+++ URLRewriting/src/main/java/myservlets/Servlet1.java
@@ -0,0 +1,47 @@
+package myservlets;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		// fetch name
+		String name=request.getParameter("name");
+		out.println("<h1> Your name is "+name+"</h1>");
+		
+	
+		out.println("<h2> <a href='servlet2?user="+name+"'> Go to Servlet2</a></h2>");
+		
+		
+		
+		
+		
+	}
+
+}
